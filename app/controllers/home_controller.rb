c7c1aca8c15700992af10d614f2ffa5f8c2dbf18
keisukesaito7 <keisukesaito7@gmail.com>
2020-10-17 23:31:01 +0900

    change root_path if signed_in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,16 @@
 class HomeController < ApplicationController
+  before_action :already_signed_in, only: [:top]
+
   def top
   end
 
   def test
+  end
+
+  private
+  def already_signed_in
+    if user_signed_in?
+      redirect_to test_path
+    end
   end
 end
