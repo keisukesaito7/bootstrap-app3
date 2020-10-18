e1180ed38fc721f5e36dbb1736ba315930ea1c07
keisukesaito7 <keisukesaito7@gmail.com>
2020-10-18 18:51:50 +0900

    edit tasks_controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,17 @@
 class TasksController < ApplicationController
+  before_action :redirect_to_root_path, only: :index
+  before_action :set_current_user, only: :index
+
   def index
+    @tasks = Task.all.order('created_at DESC')
+  end
+
+  private
+  def redirect_to_root_path
+    redirect_to root_path if user_signed_in?
+  end
+
+  def set_current_user
+    @user = User.find(current_user.id)
   end
 end
