d0e494693ff982bbdb5097750ee0104cb5f2002a
keisukesaito7 <keisukesaito7@gmail.com>
2020-11-04 16:37:51 +0900

    [テスト]ユーザー新規登録の異常系を確認

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "Users", type: :system do
     @user = FactoryBot.build(:user)
   end
 
-  describe 'ユーザー新規登録ができるとき' do
+  context 'ユーザー新規登録ができるとき' do
     it '正しい情報が入力されれば登録されトップページへ移動' do
       # トップページへ移動
       visit root_path
@@ -31,4 +31,27 @@ RSpec.describe "Users", type: :system do
       expect(page).to have_no_content("新規登録")
     end
   end
+
+  context 'ユーザー新規登録ができないとき' do
+    it '誤った情報ではユーザー新規登録できずに新規登録ページへ戻ってくる' do
+      # トップページに移動する
+      visit root_path
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content("新規登録")
+      # 新規登録ページへ移動する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      fill_in "user_name", with: ""
+      fill_in "user_email", with: ""
+      fill_in "user_password", with: ""
+      fill_in "user_password_confirmation", with: ""
+      # クリックしてモーダルを表示
+      find("button[data-target='#exampleModal']").click
+      # アカウント登録を押してもユーザーモデルのカウントは上がらないことを確認する
+      expect{ find("input[name='commit']").click }.to change{ User.count }.by(0)
+      # 新規登録ページへ戻されることを確認する
+      expect(current_path).to eq '/users'
+    end
+  end
+
 end
