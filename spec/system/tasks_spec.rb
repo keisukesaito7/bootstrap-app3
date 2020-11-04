6d5519e7f509f358af082339f3165dc8b6bd5ccf
keisukesaito7 <keisukesaito7@gmail.com>
2020-11-04 18:23:21 +0900

    [テスト]タスク新規投稿の異常系を確認

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -25,4 +25,30 @@ RSpec.describe "Tasks#Create", type: :system do
     end
   end
 
+  context 'タスクを投稿できないとき' do
+    it 'ログインしていなければタスク新規作成ページへ遷移できない' do
+      # トップページへアクセス
+      visit root_path
+      # タスク新規作成ページへアクセス
+      visit new_task_path
+      # ログインしていないのでログインページへリダイレクトされる
+      expect(current_path).to eq new_user_session_path
+    end
+
+    it 'ログインしてもフォームを正しく送信しないとタスクを投稿できない' do
+      # トップページへアクセス
+      visit root_path
+      # ログイン
+      sign_in(@user)
+      # 「新規タスク」ボタンを押して新規タスク投稿画面へ
+      find_link("新規タスク", href: new_task_path).click
+      # フォームに@task_contentを入力
+      fill_in "task_content", with: ""
+      # Create Taskを押した時にタスクモデルのカウントが上がらないことを確認
+      expect{ find("input[name='commit']").click }.to change{ Task.count }.by(0)
+      # タスク新規投稿ページに戻ってくることを確認
+      expect(current_path).to eq '/tasks'
+    end
+  end
+
 end
