eddddc80242ba1a31a683f63d4eb32ea71a38e44
keisukesaito7 <keisukesaito7@gmail.com>
2020-11-04 17:02:01 +0900

    tasks#new, createアクションのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#top'
   get '/test', to: 'home#test'
-  resources :tasks, only: [:index]
+  resources :tasks, only: [:index, :new, :create]
 end
