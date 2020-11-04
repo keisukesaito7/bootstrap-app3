0a82b08e32b1dcc5a8043823743ddb89961ae73d
keisukesaito7 <keisukesaito7@gmail.com>
2020-11-04 17:08:27 +0900

    tasks#new, createアクションを実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,21 +1,32 @@
 class TasksController < ApplicationController
-  before_action :redirect_to_root_path, only: :index
-  before_action :set_current_user, only: :index
+  before_action :authenticate_user!, only: [:index, :new, :create]
+  before_action :set_current_user, only: [:index, :new, :create]
 
   def index
     @tasks = Task.all.order('created_at DESC')
   end
 
-  private
-  def redirect_to_root_path
-    redirect_to root_path unless user_signed_in?
+  def new
+    @task = Task.new
   end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.valid?
+      @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
+  end
+
+  private
 
   def set_current_user
     @user = User.find(current_user.id)
   end
 
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content).merge(user_id: current_user.id)
   end
 end
