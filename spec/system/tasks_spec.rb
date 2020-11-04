1607fa93f9da608c0b8f4546d73815980cb7f03b
keisukesaito7 <keisukesaito7@gmail.com>
2020-11-04 18:17:39 +0900

    [テスト]タスク新規投稿の正常系を確認

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,9 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe "Tasks", type: :system do
+RSpec.describe "Tasks#Create", type: :system do
   before do
-    driven_by(:rack_test)
+    @user = FactoryBot.create(:user)
+    @task_content = Faker::Lorem.sentence
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  context 'タスクを投稿できるとき' do
+    it '正しい情報が入力されればタスクが投稿されトップページへ移動' do
+      # トップページへアクセス
+      visit root_path
+      # ログイン
+      sign_in(@user)
+      # 「新規タスク」ボタンを押して新規タスク投稿画面へ
+      find_link("新規タスク", href: new_task_path).click
+      # フォームに@task_contentを入力
+      fill_in "task_content", with: @task_content
+      # Create Taskを押して送信
+      expect{ find("input[name='commit']").click }.to change{ Task.count }.by(1)
+      # root_pathに遷移したことを確認
+      expect(current_path).to eq root_path
+      # ページ内に@task_contentがあることを確認
+      expect(page).to have_content(@task_content)
+    end
+  end
+
 end
